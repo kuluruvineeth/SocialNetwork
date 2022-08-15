7eaa352f55a1a1bd3483b7cafbe40e6fcf7aa74e
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-08-15 17:44:35 +0530

    modified dependencies to compatible versions.

--- app/src/main/java/com/kuluruvineeth/socialnetwork/ui/theme/Color.kt
+++ app/src/main/java/com/kuluruvineeth/socialnetwork/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package com.kuluruvineeth.socialnetwork.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val DarkGray = Color(0xFF202020)
+val MediumGray = Color(0xFF404040)
+val LightGray = Color(0xFF000000)
+val TextWhite = Color(0xFFEEEEEE)
+val HintGray = Color(0xFF6D6D6D)
+val GreenAccent = Color(0xFF08FF04)
