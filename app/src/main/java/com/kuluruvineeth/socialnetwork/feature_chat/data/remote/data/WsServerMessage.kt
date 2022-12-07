b233cb1bb1adeec4efbe9731a6e14d393b33949b
Kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-12-07 21:27:54 +0530

    Fixed formatting issue of sent chat messages

--- app/src/main/java/com/kuluruvineeth/socialnetwork/feature_chat/data/remote/data/WsServerMessage.kt
+++ app/src/main/java/com/kuluruvineeth/socialnetwork/feature_chat/data/remote/data/WsServerMessage.kt
@@ -4,6 +4,7 @@ package com.kuluruvineeth.socialnetwork.feature_chat.data.remote.data
 
 import com.kuluruvineeth.socialnetwork.feature_chat.domain.model.Message
 import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.util.*
 
 data class WsServerMessage(
@@ -18,9 +19,8 @@ data class WsServerMessage(
             fromId = fromId,
             toId = toId,
             text = text,
-            formattedTime = DateFormat
-                .getDateInstance(DateFormat.DEFAULT)
-                .format(Date(timestamp)),
+            formattedTime = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+                .format(timestamp),
             chatId = chatId
         )
     }
